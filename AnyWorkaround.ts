9ab8d6f44a05c5de758a7027641ca5213a26a35c
Devan Grose <devan.grose@crowdstreet.com>
2021-05-18 09:51:29 -0700

    Initial commit

---
+++ AnyWorkaround.ts
@@ -0,0 +1,18 @@
+// We want to display the user's email, given this JSON response and strict type
+
+let userJSON: any = {
+    name: "Bob",
+    occupation: "Builder",
+    title: "The",
+    email: "bobthebuilder@canwefixit.com",
+    workEthic: 9001,
+}
+
+type Person = {
+    name: string,
+    occupation: string,
+    title: string,
+    workEthic: number
+}
+
+let user: Person = userJSON;
